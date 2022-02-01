f6953e8536ed5084470d0675cf4904717b6f737e
Bevin Duncan <bevin.duncan@gmail.com>
2022-01-31 21:11:00 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock){
+    return Math.abs(42 - pickupBlock)
+    }
+    function distanceFromHqInFeet(pickupBlock){
+        return distanceFromHqInBlocks(pickupBlock)* 264
+    }
+    function distanceTravelledInFeet(start, end){
+        return Math.abs(start-end) * 264
+    }
+    function calculatesFarePrice(start, end){
+        const distance = distanceTravelledInFeet(start, end)
+        if (distance < 400){
+            return 0
+        }
+        else if (distance > 400 && distance <= 2000){
+            return (distance - 400) * 0.02
+        }
+        else if (distance > 2000 && distance < 2500){
+            return 25
+        }
+        else {
+            return 'cannot travel that far'
+        }
+    }
